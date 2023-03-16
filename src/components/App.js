1f52e5147523ae5858f4ce1573e965a0f8765032
98aj <103376356+98aj@users.noreply.github.com>
2023-03-16 22:43:54 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -5,15 +5,12 @@ import './../styles/App.css';
 
 const App = () => {
   
-  const [ct, setCt] = useState(0);
-  function increase(){
-    setCt(ct+1);
+
   }
   return (
     <div>
         {/* Do not remove the main div */}
-    <p>Button clicked {ct} times</p>
-    <button onClick={increase}>Increase</button>
+    
     </div>
   )
 }
